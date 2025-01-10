c0237e0534703d22522cf43f101518862ba84a2c
CompewterTutor <mike.d.morrissey@gmail.com>
2025-01-09 21:32:22 -0700

    oijoij

--- ExpoFrontend/app/_layout.tsx
+++ ExpoFrontend/app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SessionProvider } from '@/context/index';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SessionProvider>
+      <Slot />
+    </SessionProvider>
+  );
 }
