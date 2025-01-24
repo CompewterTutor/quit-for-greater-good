71c2b59441fffd6243c482c2e9edf5403c420b70
Michael Morrissey <mike.d.morrissey@gmail.com>
2025-01-24 14:17:15 -0700

    added react native gifted charts

--- ExpoFrontend/app/_layout.tsx
+++ ExpoFrontend/app/_layout.tsx
@@ -1,11 +1,14 @@
 import "../global.css";
 import { Slot } from "expo-router";
 import { SessionProvider } from '@/context/index';
+import { PortalHost } from '@rn-primitives/portal';
 
 export default function RootLayout() {
   return (
     <SessionProvider>
       <Slot />
+      <PortalHost />
     </SessionProvider>
+    
   );
 }
